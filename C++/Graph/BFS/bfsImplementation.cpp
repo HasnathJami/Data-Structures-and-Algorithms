bcf6b298fe3cc6dbb16aeec3217bc6f1c26a6ac1
Hasnath Jami Chowdhury <62906638+HasnathJami@users.noreply.github.com>
2021-10-28 23:55:03 +0600

    Add files via upload

---
+++ C++/Graph/BFS/bfsImplementation.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include<queue>
+#include<string.h>
+using namespace std;
+
+
+
+
+
+int main()
+{
+    vector<int> adjList[100];
+
+    int nodes,edges;
+    cin>>nodes>>edges;
+
+    for(int i=0; i<edges; i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+
+
+    bool visited[100];
+    memset(visited,false,sizeof(visited));
+
+    int startingNode=1;
+
+    queue<int>q;
+    q.push(startingNode);
+    visited[startingNode]=true;
+
+    while(q.empty()==false)
+    {
+        int x=q.front();
+        q.pop();
+
+        cout<<x<<" ";
+
+        for(int i=0; i<adjList[x].size(); i++)
+        {
+            int y=adjList[x][i];
+            if(visited[y]==0)
+            {
+                visited[y]=1;
+                q.push(y);
+            }
+        }
+    }
+    cout<<endl;
+
+
+
+}
