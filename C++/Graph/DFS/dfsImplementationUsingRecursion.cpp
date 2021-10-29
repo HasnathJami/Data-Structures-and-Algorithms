a8a8b98ec51b66bb65dce0474232665689604adf
Hasnath Jami Chowdhury <62906638+HasnathJami@users.noreply.github.com>
2021-10-29 15:59:42 +0600

    Add files via upload

---
+++ C++/Graph/DFS/dfsImplementationUsingRecursion.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <string.h>
+using namespace std;
+
+vector<int>adjList[100];
+bool visited[100];
+
+void dfs(int sn)
+{
+
+    int x=sn;
+    visited[x]=true;
+    cout<<x<<" ";
+
+
+    for(int i=0; i<adjList[x].size(); i++)
+    {
+        int y=adjList[x][i];
+        if(visited[y]==false)
+        {
+            dfs(y);
+        }
+    }
+
+
+
+}
+
+
+int main()
+{
+
+    int nodes,edges;
+    cin>>nodes>>edges;
+
+    for(int i=0; i<edges; i++)
+    {
+        int x,y;
+        cin>>x>>y;
+        adjList[x].push_back(y);
+        adjList[y].push_back(x);
+    }
+    memset(visited,false,sizeof(visited));
+
+    dfs(1);
+    cout<<endl;
+
+
+}
