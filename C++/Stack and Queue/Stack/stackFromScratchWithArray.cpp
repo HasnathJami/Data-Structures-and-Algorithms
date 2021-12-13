072e3dca8a16f76f8aad3a9fac23458da59a07c5
Hasnath Jami Chowdhury <62906638+HasnathJami@users.noreply.github.com>
2021-12-13 22:03:47 +0600

    Add files via upload

---
+++ C++/Stack and Queue/Stack/stackFromScratchWithArray.cpp
@@ -0,0 +1,104 @@
+/*
+  Stack Properties:
+  push()
+  pop()
+  top()
+  stackSize()
+  isEmpty()
+  show()
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[1000];
+int t=-1;
+int len=sizeof(arr)/sizeof(arr[0]);
+
+
+void push(int a)
+{
+    if(t==len-1)
+        cout<<"Stack is full"<<endl;
+    else
+    {
+        t++;
+        arr[t]=a;
+
+    }
+
+}
+
+void pop()
+{
+    if(t==-1)
+        cout<<"Stack is empty"<<endl;
+    t--;
+}
+
+int top()
+{
+    if(t==-1)
+    {
+        cout<<"Stack is empty"<<endl;
+        return 0;
+    }
+
+    return arr[t];
+
+
+}
+
+void stackSize()
+{
+    cout<<t<<endl;
+}
+
+void show()
+{
+    for(int i=0; i<=t; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+bool isEmpty()
+{
+    if(t<0)//(t==-1)// for return t-1 = 1-1=0
+        return false;
+
+    else
+        return true;
+}
+
+
+int main()
+{
+    int n;
+    cin>>n;
+
+
+
+    for(int i=0; i<n; i++)
+    {
+        int inp;
+        cin>>inp;
+        push(inp);
+    }
+
+
+    while(isEmpty()==true)
+    {
+        int s=top();
+        cout<<s<<" ";
+        pop();
+
+    }
+    cout<<endl;
+    cout<<top()<<endl;
+
+
+
+}
